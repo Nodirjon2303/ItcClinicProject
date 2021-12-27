2a43a9a18814c5774afb0fe55ea9b202c53d4e3f
Your Name <you@example.com>
2021-12-28 00:04:09 +0500

    nodir

--- ItcClinicProject/settings.py
+++ ItcClinicProject/settings.py
@@ -75,21 +75,21 @@ WSGI_APPLICATION = 'ItcClinicProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'HOST': 'localhost',
-#         'NAME': 'fortunabot',
-#         'PASSWORD': 'fortunabot',
-#         'USER': 'fortunabot'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'localhost',
+        'NAME': 'fortunabot',
+        'PASSWORD': 'fortunabot',
+        'USER': 'fortunabot'
+    }
+}
 
 
 
