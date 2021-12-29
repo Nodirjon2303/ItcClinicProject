892012e2a87709b087834d7890087a45d3dd05c9
Your Name <you@example.com>
2021-12-29 10:27:54 +0500

    nodir

--- ItcClinicProject/settings.py
+++ ItcClinicProject/settings.py
@@ -137,7 +137,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
