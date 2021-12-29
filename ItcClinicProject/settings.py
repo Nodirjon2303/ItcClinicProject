4d7de0e1dc146702e73e3d5bdaebbf627fc5c2b5
Your Name <you@example.com>
2021-12-29 14:43:24 +0500

    nodir

--- ItcClinicProject/settings.py
+++ ItcClinicProject/settings.py
@@ -140,6 +140,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticdjango')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
