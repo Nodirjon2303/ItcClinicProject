81c58450f3cdf160bce99b7893874ef85909d195
Your Name <you@example.com>
2023-02-17 18:48:08 +0000

    server changes

--- ItcClinicProject/settings.py
+++ ItcClinicProject/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Clinic',
-    'jazzmin'
+    'Clinic'
 ]
 
 MIDDLEWARE = [
